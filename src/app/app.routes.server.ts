40811616629870ce0cdebc3b52ac38672e32dcaf
Dane Reid <danereid@Danes-MacBook-Pro.local>
2025-05-26 22:46:46 -0400

    ready to deploy

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,16 +1,26 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { routesIDs } from './mocks/routes-id';
+import { routesIDs2 } from './mocks/routes-id-2';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
   },
-  // {
-  //   path: 'album/:albumId',
-  //   renderMode: RenderMode.Prerender,
-  //   async getPrerenderParams() {
-  //     const ids = this.params.albumId;
-  //     return ids.map(albumId => ({albumId})
-  //   },
-  // }
+  {
+    path: 'album/:albumId',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const ids = routesIDs;
+      return ids.map(id => ({id}))
+    },
+  },
+  {
+    path: 'singles-ep/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const ids = routesIDs2;
+      return ids.map(id => ({id}))
+    }
+  }
 ];
---
+++ src/app/mocks/routes-id.ts
@@ -0,0 +1,9 @@
+export const routesIDs: string[] = [
+'0',
+'1',
+'2',
+'3',
+'4',
+'5',
+'6',
+];
