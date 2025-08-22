1f88d9f960098786fd323c0eb81574f714dc14cf
danereid <danereidcode@gmail.com>
2025-08-22 16:06:53 -0400

    fixing 404 issue

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideHttpClient(withFetch()), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideHttpClient(withFetch()), provideRouter(routes), provideClientHydration(withEventReplay()), HashLocationStrategy]
 };
